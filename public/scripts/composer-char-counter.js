ed30d4c70810e3782798e0dad76a49859c11e51c
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2020-08-11 18:40:29 +0000

    Fixed tweet character counter bug, added styling for tweet card

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,17 @@ $(document).ready(function () {
   // --- our code goes here ---
 
   //New Tweet character counter
-  $('#tweet-text').on('keydown', function () {
+  $('#tweet-text').on('keyup', function () {
     const currentForm = $(this).parent();
     const charCounter = $(currentForm).find('div').find('output');
     const numOfChars = $(this).val().length;
-    if (numOfChars > 140) {
-      charCounter.toggleClass('danger');
-      charCounter.text(140 - numOfChars);
-    }else {
-      charCounter.text(numOfChars);
+    if (140 - numOfChars < 0) {
+      charCounter.addClass('danger');
+    } else{
+      charCounter.removeClass('danger');
+
     }
 
+    charCounter.text(140 - numOfChars);
   });
 });
